794867c7f6ca3a2f099d16ab7a0d73581222aa41
Brandt Tullis <brandt.tullis@kw.com>
2020-01-16 20:38:20 -0600

    works for projects and folders

--- gcp/iam-buddy.py
+++ gcp/iam-buddy.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python3
 #TODO:
-#normalize folder, project, and organization
+#load all iam into yaml
+#command line arguments
+#add organization
 
 import subprocess
 import json
 
-all_folders = []
-found_folders = []
+resources = []
+found_resources = []
 org_id=1045899897599
-target_member='anthony.tartaglia@kw.com'
+target_member='brandt.tullis@kw.com'
 
 # raw_output = subprocess.check_output(['gcloud','projects', 'list', '--format=value(name, project_id)'])
 # output = raw_output.decode("utf-8").split('\n')
@@ -24,36 +26,67 @@ def get_json(command):
     raw_output = subprocess.check_output(command)
     return json.loads(raw_output.decode("utf-8"))
 
+def normalize_folder(folder, iam):
+    normalized = {}
+    normalized['iam'] = iam
+    normalized['name'] = folder['displayName']
+    normalized['id'] = folder['name'].rsplit('/', 1)[-1]
+    normalized['parent'] = folder['parent'].rsplit('/', 1)[-1]
+    normalized['type'] = 'folder'
+    return normalized
+
 def recurse_folders(name):
     folder_id = name.rsplit('/', 1)[-1]
     folders = get_json(['gcloud', 'resource-manager', 'folders', 'list', '--folder={}'.format(folder_id), '--format=json'])
     if folders:
         for sub_folder in folders:
             iam = get_json(['gcloud', 'resource-manager', 'folders', 'get-iam-policy', '{}'.format(folder['name'].rsplit('/', 1)[-1]), '--format=json'])
-            folder['iam'] = iam
-            all_folders.append(sub_folder)
+            normalized = normalize_folder(folder, iam)
+            resources.append(normalized)
             print('Appended ' + sub_folder['displayName'])
+            print(normalized)
             recurse_folders(sub_folder['name'])
     return
 
+# get all project info and iam polices
+print('Loading project IAM polices...')
+projects = get_json(['gcloud', 'projects', 'list', '--format=json'])
+for project in projects:
+    print(project)
+    iam = get_json(['gcloud', 'projects', 'get-iam-policy', '{}'.format(project['projectId']), '--format=json'])
+    normalized = {}
+    normalized['iam'] = iam
+    normalized['name'] = project['name']
+    normalized['id'] = project['projectId']
+    try:
+        normalized['parent'] = project['parent']['id']
+    except KeyError:
+        normalized['parent'] = 'NONE'
+    normalized['type'] = 'project'
+    resources.append(normalized)
+
 # get top-level folder info
+print('Loading folder IAM polices...')
 folders = get_json(['gcloud', 'resource-manager', 'folders', 'list', '--organization={}'.format(org_id), '--format=json'])
-# recurse through each top-level folder, getting folder info and iam policies
+
+# recurse  each top-level folder, getting folder info and iam policies
 for folder in folders:
     iam = get_json(['gcloud', 'resource-manager', 'folders', 'get-iam-policy', '{}'.format(folder['name'].rsplit('/', 1)[-1]), '--format=json'])
-    folder['iam'] = iam
-    all_folders.append(folder)
+    normalized = normalize_folder(folder, iam)
+    resources.append(normalized)
+    print(normalized)
     recurse_folders(folder['name'])
 
 #prototype for finding a user
-for folder in folders:
+for resource in resources:
     try:
-        for binding in folder['iam']['bindings']:
+        for binding in resource['iam']['bindings']:
             for member in binding['members']:
                 if member.strip('user:') == target_member:
-                    if folder['displayName'] not in found_folders:
-                        found_folders.append(folder['displayName'])
+                    if resource['displayName'] not in found_resources:
+                        found_resources.append(folder['displayName'])
     except KeyError:
         pass
-for folder in found_folders:
+
+for resource in found_resources:
     print(folder + '\n')
